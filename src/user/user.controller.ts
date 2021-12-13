a1e9212b421e6a76a9e0764ea94feae380bdaf1b
Duy Nguyen <ducduy.ababat@gmail.com>
2021-12-13 19:11:37 +0700

    user controller includes getting token and registering new user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Req,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.request';
+import { Request } from 'express';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  public async registerUser(@Body() user: CreateUserDto) {
+    try {
+      await this.userService.createUser(user);
+    } catch (e) {
+      throw new HttpException(
+        {
+          code: e.code,
+          message: e.message,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Get()
+  public async getToken(@Req() request: Request) {
+    const username: string = request.header('Firebase-User');
+
+    if (!username) {
+      throw new HttpException(
+        {
+          message:
+            'No user is provided in header. Please provide username under "Firebase-User"',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return await this.userService.getToken(username);
+  }
+}
